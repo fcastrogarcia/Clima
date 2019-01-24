12b8a218932f2a411d04e6dfe05463c1b8be9e8f
fcastrogarcia <fede.castrog@gmail.com>
2019-01-24 19:37:46 -0300

    Updates

---
+++ precache-manifest.b672bf7da7a3a62058f378fe7de67f5a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "77bdbaa8d80f2a3a1098",
+    "url": "/Clima/static/js/runtime~main.77bdbaa8.js"
+  },
+  {
+    "revision": "4aaf96f15ca1a3e4db92",
+    "url": "/Clima/static/js/main.4aaf96f1.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/Clima/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "4aaf96f15ca1a3e4db92",
+    "url": "/Clima/static/css/main.da86cc58.chunk.css"
+  },
+  {
+    "revision": "728f9e0b6ec140af2fe98bfb5e1b51be",
+    "url": "/Clima/index.html"
+  }
+];
